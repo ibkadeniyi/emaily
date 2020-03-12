afb724bba962f164d750ff3b42fbd4b9eb48ff93
Ibukun Adeniyi <ibukunadeniyi@Ibukuns-MacBook-Pro.local>
2020-03-12 22:37:06 +0100

    tweak prod js

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 //production keys here
-module.exports= {
+module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
