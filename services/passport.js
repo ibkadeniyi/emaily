83bd966858dd032c0160fa34a08de0ae3b01ae19
Ibukun Adeniyi <ibukunadeniyi@Ibukuns-MacBook-Pro.local>
2020-03-14 02:30:10 +0100

    react and redux beginning

--- services/passport.js
+++ services/passport.js
@@ -12,29 +12,28 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser((id, done) => {
     User.findById(id)
-    .then(user => {
-        done(null, user);
-    });
+        .then(user => {
+            done(null, user);
+        });
 });
 
 passport.use(
     new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: true
-    }, 
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({googleID: profile.id}).then(existingUser => {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback',
+        proxy: true
+    },
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleID: profile.id })
+
             if (existingUser) {
                 //this user already exists
                 done(null, existingUser);
-            }else {
-                    new User({ googleID: profile.id})
-                    .save()
-                    .then(user => done(null, user));
-                }
-            
-        })
-    })
+            } else {
+                const user = await new User({ googleID: profile.id }).save()
+                done(null, user);
+            }
+        }
+    )
 );
